cb087754403a5b34a32d961efdee6d2b69fa4ea2
Patrick Logan <jpatricklogan@gmail.com>
2017-05-22 12:52:26 -0600

    Initial creation to transpose old code to Create-React-App framework

---
+++  src_old_code/app/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import React from 'react';
+import {render} from 'react-dom';
+
+import Routing from './components/Routing'
+
+const app = document.getElementById('app');
+
+render(<Routing />, app);
